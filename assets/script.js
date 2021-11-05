ac1c318e8b7eadb027886a35544affd09a3cf847
Lidija Ladic <lidija.ladic@gmail.com>
2021-11-05 14:39:05 +0100

    current working version

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,8 @@
+//global function (for now), takes array of objets as parameter and returns random object
+// (in case here takes questions as parameter and returns random question)
+const chooseRandomQuestion = questions => questions[Math.round(Math.random()*questions.length-1)];
+
+
 $(document).ready(function(){
 	//fades in the start page
     $(".fadeIn").delay('500').fadeIn("slow");
@@ -12,11 +17,13 @@ $(document).ready(function(){
     		//console.log(questionsData);
 
     		 //take a random question out of data
-            const randomQuestion = questionsData[Math.round(Math.random()*questionsData.length-1)];
-            let points = 0;
-            //console.log(randomQuestion);
+            let randomQuestion = chooseRandomQuestion(data);
+            console.log(randomQuestion);
 
-    		const answersArr = []; //save answers as objects, containing question id and answer id for answer comparison
+            //count points
+            let points = 0;
+          
+    		//const answersArr = []; //save answers as objects, containing question id and answer id for answer comparison
     		//or immediately compare answer id with questions correct value, add a point and turn text green/yellow,
     		//depending if the answer is correct or not
 
@@ -26,17 +33,14 @@ $(document).ready(function(){
 
     		//create paragraph that will contain a question and append it to form
     		const questionParagraph = $("<p></p>");
-            const nextButton = $("<button>Next</button>");
+            const nextButton = $("<button id='next'>Next</button>");
     		questionForm.append(questionParagraph);
 
     		//save question to paragraph
             questionParagraph.html(randomQuestion.question);
-
-         	//show the question form
-            questionForm.delay('600').fadeIn('slow');
           
           	//print out answers from the random question
-            randomQuestion.answers.forEach(answer => {
+            randomQuestion.answers.forEach(answer => { //make global function from this?
                 console.log(answer);
                 questionForm.append("<input type='radio' id='" + answer.id 
                     + "' name='answer' value='" + answer.value 
@@ -45,6 +49,9 @@ $(document).ready(function(){
             });
 
             questionForm.append(nextButton);
+
+            //show the question form
+            questionForm.delay('600').fadeIn('slow');
 
             //check selection with click on the next button
             nextButton.click(function() {
@@ -64,7 +71,51 @@ $(document).ready(function(){
 				});
                 if (!anyChecked) {
                     alert('Please select your answer!');
-                } else {} //repeat the process, hide question and load new question
+                } else {
+                    questionForm.fadeOut();
+                    $(questionForm).promise().done(function(){
+                        // will be called when all the animations on the queue finish
+                        randomQuestion = chooseRandomQuestion(data);
+                        console.log(randomQuestion);
+                        questionParagraph.html(randomQuestion.question);
+                        anyChecked = false;
+                        //clean existing answers
+                        $('input').remove();
+                        $('label').remove();
+                        nextButton.remove();
+
+                        randomQuestion.answers.forEach(answer => { //make global function from this?
+                            console.log(answer);
+                            questionForm.append("<input type='radio' id='" + answer.id 
+                                + "' name='answer' value='" + answer.value 
+                                + "'><label for='" + answer.value + "'>" 
+                                + answer.value + "</label><br>");
+                        });
+
+                        questionForm.append(nextButton);
+                        //show the question form
+                        questionForm.fadeIn('slow');
+        //handling second question
+                        nextButton.click(function() {
+                            event.preventDefault();
+                            $('input').each(function() {
+                                if ($(this).is(':checked')) {
+                                    if(this.id == randomQuestion.correct) {
+                                        points++;
+                                        console.log(`Correct! You have ${points} points.`);
+                                        anyChecked = true;
+                                    } else {
+                                        console.log(`Wrong answer! The correct answer is ${randomQuestion.correct}.`);
+                                        anyChecked = true;
+                                    }
+                                }
+                            });
+                        });   
+                    });
+                    
+
+
+                } //repeat the process, hide question and load new question
             });
             
 
